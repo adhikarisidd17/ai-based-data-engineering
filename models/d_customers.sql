250ecfe26f019b637b987337d6669972277dd0b9
Siddharth Adhikari <93575478+adhikarisidd17@users.noreply.github.com>
2025-06-11 16:19:52 +0200

    formatting

--- models/d_customers.sql
+++ models/d_customers.sql
@@ -1,13 +1,13 @@
 select 
-    name as customer_name,
-       email as customer_email,
-       revenue as revenue,
-       years,
-       phone as customer_phone,
-       address as customer_address,
-       city as customer_city,
-       state as customer_state,
-       zip_code as customer_zip_code,
-       country as customer_country
-       from {{ ref('stg_customers') }}
-       where is_active = true
+name as customer_name,
+email as customer_email,
+revenue as revenue,
+years,
+phone as customer_phone,
+address as customer_address,
+city as customer_city,
+state as customer_state,
+zip_code as customer_zip_code,
+country as customer_country
+from {{ ref('stg_customers') }}
+where is_active = true
