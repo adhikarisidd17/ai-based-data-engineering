418bd4c861e00909b6737a3dfc0a999e67b5db03
Siddharth Adhikari <93575478+adhikarisidd17@users.noreply.github.com>
2025-06-11 17:12:49 +0200

    feat(d_customers): Add a new column called customer_age in d_customers

--- models/d_customers.sql
+++ models/d_customers.sql
@@ -1,3 +1,4 @@
+```sql
 select 
 name as customer_name,
 email as customer_email,
@@ -8,6 +9,8 @@ address as customer_address,
 city as customer_city,
 state as customer_state,
 zip_code as customer_zip_code,
-country as customer_country
+country as customer_country,
+current_date - date_of_birth as customer_age  -- Assuming there is a date_of_birth column in stg_customers
 from {{ ref('stg_customers') }}
 where is_active = true
+```
