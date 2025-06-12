59bb8afb070e3dff242c64845eb43741a7e671ed
Siddharth Adhikari <93575478+adhikarisidd17@users.noreply.github.com>
2025-06-12 22:54:07 +0200

    chore: update models/d_customers.sql per request

--- models/d_customers.sql
+++ models/d_customers.sql
@@ -1,8 +1,9 @@
 select
-  id,
-  name,
-  email,
-  phone_number,
-  dob
+    id,
+    name,
+    email,
+    phone_number,
+    dob,
+    datediff(current_date, dob) as age
 from
-  customers
+    customers
