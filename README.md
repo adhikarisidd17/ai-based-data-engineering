987d5c481dc8df5abc472287522172c339e265d2
Siddharth Adhikari <93575478+adhikarisidd17@users.noreply.github.com>
2025-06-12 14:20:42 +0200

    Updating readme

---
+++ README.md
@@ -0,0 +1,5 @@
+Run the framework by running:
+
+poetry install
+
+poetry run uvicorn ai_dbt_bot.main:app --reload --port 8000 --log-level debug
--- batch_export_tests/metadata_tests.py
+++ batch_export_tests/metadata_tests.py
@@ -1,4 +1,4 @@
-from batch_export_tests.metadata_models import FileExportsMetadata, FileExportsMetadataList, KafkaMetadata, KafkaMetadataList
+from batch_export_tests.metadata_models import FileExportsMetadata, FileExportsMetadataList
 
 def test_file_exports_metadata_collection(file_exports_metadata):
     FileExportsMetadataList(rows=file_exports_metadata)
@@ -7,11 +7,3 @@ def test_file_exports_metadata(file_exports_metadata):
     for integration in file_exports_metadata:
         print(integration)
         FileExportsMetadata(**integration)
-
-def test_kafka_metadata_collection(kafka_metadata):
-    KafkaMetadataList(rows=kafka_metadata)
-
-def test_kafka_metadata(kafka_metadata):
-    for integration in kafka_metadata:
-        print(integration)
-        KafkaMetadata(**integration)
