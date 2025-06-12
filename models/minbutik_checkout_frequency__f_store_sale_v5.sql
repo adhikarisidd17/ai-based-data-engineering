65e7155de6db84f59c8b34bc4745ecbc68dd1c58
Siddharth Adhikari <93575478+adhikarisidd17@users.noreply.github.com>
2025-06-12 16:11:50 +0200

    Adding dbt

---
+++ models/minbutik_checkout_frequency__f_store_sale_v5.sql
@@ -0,0 +1,39 @@
+
+{% set sale_receipt_line = source('dbt_sidd','minbutik_checkout_frequency__f_store_sale_v4') %}
+select
+    f.store_id,
+    f.d_date_sale,
+    f.sale_datetime_rounded,
+    f.sale_hour,
+    f.store_department_id,
+    f.store_department_name,
+    f.checkout_station_id,
+    f.checkout_method_type_name_sw,
+    f.checkout_method_code,
+    f.checkout_method_name_sw,
+    f.f_sale_receipt_key,
+    f.is_receipt_store_sale,
+    f.d_store_cashier_key,
+    f.is_receipt_returns_only,
+    sum(f.number_of_items) as number_of_items,
+    sum(f.store_sale_amount) as sale_amount_excl_vat,
+    sum(f.store_sale_vat_amount) as sale_vat_amount,
+    sum(f.commission_amount) as commission_amount_excl_vat,
+    sum(f.commission_vat_amount) as commission_vat_amount,
+    sum(f.margin_amount) as margin_amount
+from sale_receipt_line as f
+group by
+    f.store_id,
+    f.d_date_sale,
+    f.sale_datetime_rounded,
+    f.sale_hour,
+    f.store_department_id,
+    f.store_department_name,
+    f.checkout_station_id,
+    f.checkout_method_type_name_sw,
+    f.checkout_method_code,
+    f.checkout_method_name_sw,
+    f.f_sale_receipt_key,
+    f.is_receipt_store_sale,
+    f.d_store_cashier_key,
+    f.is_receipt_returns_only
