719ed10b8e6502e0a6cd37b4f198ae0eafc4f72c
Siddharth Adhikari <93575478+adhikarisidd17@users.noreply.github.com>
2025-06-18 23:36:45 +0200

    chore: update models/d_customers.sql per request

--- models/d_customers.sql
+++ models/d_customers.sql
@@ -1,8 +1,13 @@
 select
-  id,
-  name,
-  email,
-  phone_number,
-  dob
+    id,
+    name,
+    email,
+    phone_number,
+    dob,
+    case
+        when purchase_count > 10 then 'Gold'
+        when purchase_count > 5 then 'Silver'
+        else 'Bronze'
+    end as loyalty
 from
-  customers
+    customers
