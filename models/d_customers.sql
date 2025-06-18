a1ced39c521f2485a282a059817dce8f0beb9e92
Siddharth Adhikari <93575478+adhikarisidd17@users.noreply.github.com>
2025-06-19 00:49:13 +0200

    chore: update models/d_customers.sql per request

--- models/d_customers.sql
+++ models/d_customers.sql
@@ -7,3 +7,23 @@ select
     customer_lifetime_value
 from
     customers
+
+-- Updated models/customer.yml
+version: 2
+
+models:
+  - name: customer
+    description: "Customer data model"
+    columns:
+      - name: id
+        description: "Unique identifier for the customer"
+      - name: name
+        description: "Name of the customer"
+      - name: email
+        description: "Email address of the customer"
+      - name: phone_number
+        description: "Phone number of the customer"
+      - name: dob
+        description: "Date of birth of the customer"
+      - name: customer_lifetime_value
+        description: "Estimated lifetime value of the customer"
