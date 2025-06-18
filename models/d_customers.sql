c48ff4e1b1c4442c6f4bdaa44d4099768350633f
Siddharth Adhikari <93575478+adhikarisidd17@users.noreply.github.com>
2025-06-18 23:15:12 +0200

    chore: update models/d_customers.sql per request

--- models/d_customers.sql
+++ models/d_customers.sql
@@ -3,6 +3,26 @@ select
   name,
   email,
   phone_number,
-  dob
+  dob,
+  created_at
 from
   customers
+
+version: 2
+
+models:
+  - name: customer
+    description: "Customer information"
+    columns:
+      - name: id
+        description: "Unique identifier for the customer"
+      - name: name
+        description: "Name of the customer"
+      - name: email
+        description: "Email address of the customer"
+      - name: phone_number
+        description: "Phone number of the customer"
+      - name: dob
+        description: "Date of birth of the customer"
+      - name: created_at
+        description: "Timestamp when the customer was created"
