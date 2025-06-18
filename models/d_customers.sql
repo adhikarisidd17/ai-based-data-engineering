85bb1bf8064428fe1dee6c0d7b189bd44d5c55ce
Siddharth Adhikari <93575478+adhikarisidd17@users.noreply.github.com>
2025-06-18 16:07:13 +0200

    chore: update models/d_customers.sql per request

--- models/d_customers.sql
+++ models/d_customers.sql
@@ -1,8 +1,12 @@
 select
-  id,
-  name,
-  email,
-  phone_number,
-  dob
+    id,
+    name,
+    email,
+    phone_number,
+    dob,
+    case
+        when num_of_customers = 0 then null
+        else sales / num_of_customers
+    end as avg_sales
 from
-  customers
+    customers
