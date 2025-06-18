fa7bf47aa3d38a91abe87c97a6e403e7e23e7ba5
Siddharth Adhikari <93575478+adhikarisidd17@users.noreply.github.com>
2025-06-19 00:28:04 +0200

    chore: update models/d_customers.sql per request

--- models/d_customers.sql
+++ models/d_customers.sql
@@ -1,37 +1,13 @@
 select
-  id,
-  name,
-  email,
-  phone_number,
-  dob,
-  CASE 
-    WHEN purchase_count > 10 THEN 'Gold'
-    WHEN purchase_count BETWEEN 5 AND 10 THEN 'Silver'
-    ELSE 'Bronze'
-  END AS loyalty
+    id,
+    name,
+    email,
+    phone_number,
+    dob,
+    case
+        when purchase_count > 10 then 'Gold'
+        when purchase_count between 5 and 10 then 'Silver'
+        else 'Bronze'
+    end as loyalty
 from
-  customers
-
----
-
-version: 2
-
-models:
-  - name: customer
-    description: "Customer information including loyalty status."
-    columns:
-      - name: id
-        description: "Unique identifier for the customer."
-      - name: name
-        description: "Name of the customer."
-      - name: email
-        description: "Email address of the customer."
-      - name: phone_number
-        description: "Phone number of the customer."
-      - name: dob
-        description: "Date of birth of the customer."
-      - name: loyalty
-        description: "Loyalty status of the customer based on purchase history."
-        tests:
-          - not_null
-          - unique
+    customers
