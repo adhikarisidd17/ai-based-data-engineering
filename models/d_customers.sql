ba04e351bc9010b631f3bb312fc3c6f4a0f44538
Siddharth Adhikari <93575478+adhikarisidd17@users.noreply.github.com>
2025-06-18 23:33:18 +0200

    chore: update models/d_customers.sql per request

--- models/d_customers.sql
+++ models/d_customers.sql
@@ -7,3 +7,23 @@ select
     loyalty
 from
     customers
+
+-- Updated schema for customers model
+version: 2
+
+models:
+  - name: customers
+    description: "Customer data"
+    columns:
+      - name: id
+        description: "Unique identifier for the customer"
+      - name: name
+        description: "Name of the customer"
+      - name: email
+        description: "Email address of the customer"
+      - name: phone_number
+        description: "Phone number of the customer"
+      - name: dob
+        description: "Date of birth of the customer"
+      - name: loyalty
+        description: "Loyalty status of the customer"
