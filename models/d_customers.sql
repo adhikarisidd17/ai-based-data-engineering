1589593e19ab6d268876484d37cc8d20e7632a64
Siddharth Adhikari <93575478+adhikarisidd17@users.noreply.github.com>
2025-06-18 23:07:51 +0200

    chore: update models/d_customers.sql per request

--- models/d_customers.sql
+++ models/d_customers.sql
@@ -3,6 +3,28 @@ select
   name,
   email,
   phone_number,
-  dob
+  dob,
+  date_of_birth as date_of_birth
 from
   customers
+
+---
+
+version: 2
+
+models:
+  - name: customers
+    description: "This model contains customer information."
+    columns:
+      - name: id
+        description: "The unique identifier for each customer."
+      - name: name
+        description: "The name of the customer."
+      - name: email
+        description: "The email address of the customer."
+      - name: phone_number
+        description: "The phone number of the customer."
+      - name: dob
+        description: "The date of birth of the customer."
+      - name: date_of_birth
+        description: "Calculated date of birth for the customer."
