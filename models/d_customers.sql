93dcda60210b98f7e73fabea6f00b4068b1c4e3b
Siddharth Adhikari <93575478+adhikarisidd17@users.noreply.github.com>
2025-06-18 23:52:19 +0200

    chore: update models/d_customers.sql per request

--- models/d_customers.sql
+++ models/d_customers.sql
@@ -3,6 +3,27 @@ select
   name,
   email,
   phone_number,
-  dob
+  dob,
+  address
 from
   customers
+
+version: 2
+
+models:
+  - name: customer
+    description: "Customer information"
+    columns:
+      - name: id
+        description: "Unique identifier for the customer"
+      - name: name
+        description: "Name of the customer"
+      - name: email
+        description: "Email address of the customer"
+      - name: phone_number
+        description: "Phone number of the customer"
+      - name: dob
+        description: "Date of birth of the customer"
+      - name: address
+        description: "Address of the customer"
+        data_type: "string"
