3d2c691fbd7fb00339f5b3ae2043cc1b1303ea0c
Siddharth Adhikari <93575478+adhikarisidd17@users.noreply.github.com>
2025-06-19 00:24:35 +0200

    chore: update models/d_customers.sql per request

--- models/d_customers.sql
+++ models/d_customers.sql
@@ -1,17 +1,9 @@
 id: 
   description: "Unique identifier for the customer"
-  tests:
-    - unique
-    - not_null
 name: 
   description: "Name of the customer"
-  tests:
-    - not_null
 email: 
   description: "Email address of the customer"
-  tests:
-    - unique
-    - not_null
 phone_number: 
   description: "Phone number of the customer"
 dob: 
