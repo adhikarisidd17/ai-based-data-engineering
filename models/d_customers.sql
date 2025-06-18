d8e3083b639a4c221c0244d46d5894dc4975ef61
Siddharth Adhikari <93575478+adhikarisidd17@users.noreply.github.com>
2025-06-18 23:12:35 +0200

    chore: update models/d_customers.sql per request

--- models/d_customers.sql
+++ models/d_customers.sql
@@ -3,6 +3,27 @@ select
   name,
   email,
   phone_number,
-  dob
+  dob,
+  loyalty
 from
   customers
+
+columns:
+  - name: id
+    description: "The unique identifier for a customer"
+    data_type: "integer"
+  - name: name
+    description: "The name of the customer"
+    data_type: "string"
+  - name: email
+    description: "The email address of the customer"
+    data_type: "string"
+  - name: phone_number
+    description: "The phone number of the customer"
+    data_type: "string"
+  - name: dob
+    description: "The date of birth of the customer"
+    data_type: "date"
+  - name: loyalty
+    description: "The loyalty status of the customer"
+    data_type: "string"
