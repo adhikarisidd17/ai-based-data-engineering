925e07c463065b7f75901c7a3476edc7836284cf
Siddharth Adhikari <93575478+adhikarisidd17@users.noreply.github.com>
2025-06-18 22:23:25 +0200

    chore: update models/d_customers.sql per request

--- models/d_customers.sql
+++ models/d_customers.sql
@@ -3,6 +3,37 @@ select
   name,
   email,
   phone_number,
-  dob
+  dob as date_of_birth
 from
   customers
+
+version: 2
+models:
+  - name: customer
+    columns:
+      - name: id
+        tests:
+          - unique
+          - not_null
+      - name: name
+        tests:
+          - not_null
+      - name: email
+        tests:
+          - not_null
+          - unique
+      - name: phone_number
+        tests:
+          - not_null
+      - name: date_of_birth
+        tests:
+          - not_null
+
+select
+  id,
+  name,
+  email,
+  phone_number,
+  dob as date_of_birth
+from
+  source_table_name
