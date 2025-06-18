21862d0885308b8bd0c97be45603ee4934e8d1dc
Siddharth Adhikari <93575478+adhikarisidd17@users.noreply.github.com>
2025-06-18 23:18:01 +0200

    chore: update models/d_customers.sql per request

--- models/d_customers.sql
+++ models/d_customers.sql
@@ -3,6 +3,27 @@ select
   name,
   email,
   phone_number,
-  dob
+  dob,
+  login
 from
   customers
+
+columns:
+  - name: id
+    description: "The unique identifier for the customer."
+    data_type: "integer"
+  - name: name
+    description: "The name of the customer."
+    data_type: "string"
+  - name: email
+    description: "The email address of the customer."
+    data_type: "string"
+  - name: phone_number
+    description: "The phone number of the customer."
+    data_type: "string"
+  - name: dob
+    description: "The date of birth of the customer."
+    data_type: "date"
+  - name: login
+    description: "The login identifier for the customer."
+    data_type: "string"
