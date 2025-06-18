41abb8799354a8c61bae64e8765873dc19cd9617
Siddharth Adhikari <93575478+adhikarisidd17@users.noreply.github.com>
2025-06-18 14:38:45 +0200

    chore: update models/d_customers.sql per request

--- models/d_customers.sql
+++ models/d_customers.sql
@@ -1,9 +1,18 @@
-select
-    id,
-    name,
-    email,
-    phone_number,
-    dob,
-    sale_vat_amount / number_of_items as average_sales
-from
-    customers
+version: 2
+
+models:
+  - name: customer
+    description: "Customer data"
+    columns:
+      - name: id
+        description: "Unique identifier for the customer"
+      - name: name
+        description: "Name of the customer"
+      - name: email
+        description: "Email address of the customer"
+      - name: phone_number
+        description: "Phone number of the customer"
+      - name: dob
+        description: "Date of birth of the customer"
+      - name: average_sales
+        description: "Average sales calculated as sale_vat_amount divided by number_of_items"
